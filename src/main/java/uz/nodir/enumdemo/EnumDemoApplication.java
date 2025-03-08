d1097ee8f9ddfa1fd5de3516aa10209ecce9ab27
Home-PC <davrbankit@gmail.com>
2025-03-08 23:26:12 +0500

    first commit;

---
+++ src/main/java/uz/nodir/enumdemo/EnumDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.nodir.enumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnumDemoApplication.class, args);
+    }
+
+}
